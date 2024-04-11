763123dbfb2af0737d92d069460ff608d95a9726
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-04-11 16:54:52 +0300

    start, first step

---
+++ test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Button :class="['large']">Hello</Button>
+</template>
+
+<script setup>
+import { Button } from 'npo-sep-ui';
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
