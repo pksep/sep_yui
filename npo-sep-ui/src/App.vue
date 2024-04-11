b9d99acc11879969b4fc2ae32baf9809ad7cd928
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-04-11 16:53:42 +0300

    start, first step

---
+++ npo-sep-ui/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text">Hello</h1>
+</template>
+
+<style lang="scss">
+.text {
+  color: red;
+
+  &:hover {
+    color: blue;
+  }
+}
+</style>
