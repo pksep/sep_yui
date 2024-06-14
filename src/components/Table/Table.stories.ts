91935aec677e8fa6af9b5c90797b5a2ffe2fe7aa
Andrew Izosimov <chernigovsky108@gmail.com>
2024-06-14 17:00:59 +0300

    #sep-368 fix: fixes breadcrumbs, table created

--- src/components/Table/Table.stories.ts
+++ src/components/Table/Table.stories.ts
@@ -1,16 +1,10 @@
 import { StoryFn, Meta } from '@storybook/vue3';
 import Table from './Table.vue';
-import { StorybookControl } from '../../common/storybook';
 import { fn } from '@storybook/test';
 
 const meta = {
   title: 'Table/Table',
   component: Table,
-  argTypes: {
-    defaultIndex: {
-      control: { type: StorybookControl.number }
-    }
-  },
   args: { onClick: fn() },
   tags: ['autodocs']
 } as Meta<typeof Table>;
