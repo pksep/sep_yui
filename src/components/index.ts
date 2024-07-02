ffd0ddbd05ad4f70e14278b19afed9f0c6390404
david.perov60@gmail.com <david.perov60@gmail.com>
2024-07-02 14:51:35 +0300

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,27 +1,3 @@
-import Badges from './Badges/Badges.vue';
-import BreadCrumbs from './BreadCrumbs/BreadCrumbs.vue';
-import Button from './Button/Button.vue';
-import Switch from './Switch/Switch.vue';
-import Dropdown from './Dropdown/Dropdown.vue';
-import Filter from './Filter/Filter.vue';
-import Icon from './Icon/Icon.vue';
-import Search from './Search/Search.vue';
-import Slider from './Slider/Slider.vue';
-import Table from './Table/Table.vue';
-import Toggle from './Toggle/Toggle.vue';
-import UserMenu from './UserMenu/UserMenu.vue';
+import Button from './Button/';
 
-export {
-  Badges,
-  Button,
-  Icon,
-  Switch,
-  BreadCrumbs,
-  Dropdown,
-  Filter,
-  Search,
-  Slider,
-  Toggle,
-  Table,
-  UserMenu
-};
+export { Button };
