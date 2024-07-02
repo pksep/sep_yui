75e8e17c6b67a07b06a4963fa6c7a002e9338429
david.perov60@gmail.com <david.perov60@gmail.com>
2024-07-02 15:49:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/scss/variables.scss";`
+        additionalData: `@import "@/assets/scss/_global.scss";`
       }
     }
   }
