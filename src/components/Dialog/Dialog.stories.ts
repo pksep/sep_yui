1c461b21c7a98397382e069e186b3b7ed1733593
kogeletey <kg@re128.org>
2024-08-26 16:17:45 +0300

    \#sep-547 feat: added  dialog component

---
+++ src/components/Dialog/Dialog.stories.ts
@@ -0,0 +1,47 @@
+import { StoryFn, Meta } from '@storybook/vue3';
+import { StorybookControlEnum } from '../../common/storybook';
+import Dialog from './Dialog.vue';
+import Button from "@/components/Button/Button.vue"
+import { action } from '@storybook/addon-actions';
+import { userEvent, within } from '@storybook/test';
+
+const meta = {
+  title: 'Dialog/Dialog',
+  component: Dialog,
+  argTypes: {
+    open: {
+      control: { type: StorybookControlEnum.boolean },
+      defaultValue: true
+    }
+  },
+  tags: ['autodocs']
+} as Meta<typeof Dialog>;
+
+export default meta;
+
+const Template: StoryFn<typeof Dialog> = args => ({
+  components: { Dialog, Button },
+  setup() {
+    return { args };
+  },
+  template: `
+        <Button @click="args.open=true"> Open Dialog </Button>
+        <Dialog v-bind="args" :open="args.open"/>
+  `
+});
+
+export const Default = Template.bind({});
+
+Default.args = {
+  onClick: action('click')
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const buttonOpen = canvas.getByRole('button');
+
+  await userEvent.click(buttonOpen);
+  await userEvent.click(buttonOpen, {
+    delay: 5000
+  });
+};
---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,62 @@
+<template>
+  <dialog ref="dialog" v-bind="attrs" class="dialog-yui-kit">
+    <slot />
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, useAttrs, watchEffect } from 'vue';
+import { onClickOutside } from '@vueuse/core'
+import type { IDialogProps } from './interface/interface';
+
+const props = defineProps<IDialogProps>();
+const attrs = useAttrs();
+const dialog = ref<HTMLDialogElement>();
+const visible = ref(false);
+
+defineExpose({
+  show: showDialog,
+  close: (returnVal?: string): void => dialog.value?.close(returnVal),
+  visible
+});
+
+function showDialog() {
+  if (props.open) {
+    dialog.value?.showModal();
+  } else {
+    dialog.value.close();
+  }
+}
+
+onClickOutside(
+  visible,
+  (event) => {
+    dialog.value.close()
+  },
+)
+
+onMounted(() => {
+  watchEffect(() => {
+    if (props.open !== visible.value) {
+      showDialog();
+      visible.value = props.open;
+    }
+    });
+});
+</script>
+
+<style lang="scss" scoped>
+.dialog-yui-kit {
+  background: $BLUE-F2F7FF;
+  border: none;
+  padding: 8em;
+  border-radius: 5px;
+  &:focus-visible {
+    outline: none;
+  }
+  &[open]::backdrop {
+    background-color: $BLACK;
+    opacity: 0.4;
+  }
+}
+</style>
