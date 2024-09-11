96b933b751b282d84c2689a4d98658c208a9e786
Timofey Rumantsev <kg@re128.org>
2024-09-11 16:23:40 +0300

    feat: install inter font

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
+import '@fontsource-variable/inter';
 
 const piniaStore = createPinia();
 const app = createApp(App);
