6a462edf0781cd54b825bafc0c31dbe49d4e5ea8
Konrad Geletey <39346864+kogeletey@users.noreply.github.com>
2024-09-11 10:26:56 +0300

    feat: DradAnDrop component transferred to our library (#32)

---
+++ src/components/DragAndDrop/DradAnDrop.stories.ts
@@ -0,0 +1,27 @@
+import { StoryFn } from '@storybook/vue3';
+import DragAndDrop from './DragAndDrop.vue';
+
+const meta = {
+  title: 'DragAndDrop/DragAndDrop',
+  component: DragAndDrop,
+  tags: ['autodocs'],
+  parameters: {
+    backgrounds: { default: '#E3E3E3'}
+  }
+};
+
+export default meta;
+
+const Template: StoryFn<typeof DragAndDrop> = args => ({
+  components: { DragAndDrop },
+  setup() {
+    return { args };
+  },
+  template: `
+        <div :style="{height: '50px', width: '30rem' }">
+            <DragAndDrop v-bind="args" />
+        </div>
+     `
+});
+
+export const Default = Template.bind({});
---
+++ src/components/DragAndDrop/DragAndDrop.vue
@@ -0,0 +1,121 @@
+<template>
+  <div
+    @dragover="dragover"
+    @dragleave="dragleave"
+    @drop="drop"
+    class="dnd-yui-kit"
+  >
+    <label
+      for="docsFileSelected"
+      class="dnd-yui-kit__label"
+      :class="{ pressed: state.isPressed }"
+      @click="state.isPressed = !state.isPressed"
+    >
+      <Icon name="paperClip" stroke-width="2" />
+      <span class="dnd-yui-kit__span"> Кликните или перенесите файлы </span>
+    </label>
+    <input
+      id="docsFileSelected"
+      @change="e => addDock()"
+      type="file"
+      style="display: none"
+      required
+      :multiple="!props.IsOne"
+      ref="fileRef"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, onMounted } from 'vue';
+import { IDragAndDropProps } from './interface/interface.ts';
+import Icon from "../Icon/Icon.vue";
+
+const props = withDefaults(defineProps<IDragAndDropProps>(), {});
+
+const emits = defineEmits(['unmount']);
+
+const state = reactive({
+  isPressed: false,
+  docFiles: []
+});
+
+const fileRef = ref(null);
+
+const onChange = () => {
+  state.docFiles = [...fileRef.value.files];
+};
+
+const dragover = event => {
+  event.preventDefault();
+  const label = event.currentTarget.children[0];
+  if (!label.classList.contains('active')) label.classList.add('active');
+};
+
+const dragleave = event => {
+  event.currentTarget.children[0].classList.remove('active');
+};
+
+const drop = async event => {
+  event.preventDefault();
+  fileRef.value.files = event.dataTransfer.files;
+  await addDock();
+  event.currentTarget.children[0].classList.remove('active');
+};
+
+const addDock = async () => {
+  state.isPressed = false;
+  onChange();
+  await emits('unmount', state.docFiles);
+};
+
+onMounted(() => {
+  fileRef.value.addEventListener('cancel', () => {
+    state.isPressed = false;
+  });
+});
+</script>
+
+<style scoped>
+div.dnd-yui-kit,
+label.dnd-yui-kit__label {
+  margin-top: 16px;
+  width: 100%;
+  height: 100%;
+  background: white;
+}
+
+div.dnd-yui-kit label.dnd-yui-kit__label {
+  display: flex;
+  background: white;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  border-radius: none;
+  font-size: 20px;
+  color: #a6a3ad;
+  margin-left: 1px;
+  svg {
+    margin-right: 10px;
+  }
+}
+
+div.dnd-yui-kit label.dnd-yui-kit__label {
+  &:hover,
+  &.pressed {
+    border: 1.5px solid #77a6ff;
+    background: #f9fbff;
+  }
+  &.active,
+  &.pressed {
+    & span.dnd-yui-kit__span,
+    svg {
+      color: #77a6ff;
+    }
+  }
+  &.active {
+    border: 2.5px dotted #77a6ff;
+    background: #f9fbff;
+  }
+}
+</style>
