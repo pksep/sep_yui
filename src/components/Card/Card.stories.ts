bdaff41630984ebed30b6b730856ae8721e495a6
Konrad Geletey <39346864+kogeletey@users.noreply.github.com>
2024-09-13 14:21:55 +0300

    Yui 1797 Card component (#35)
    * feat: create a component of card
    
    * fix: simplify dialog element
    
    * feat: Card elements pressed state and hover
    
    * feat: export new components: Dialog, DragAndDrop, Card

---
+++ src/components/Card/Card.stories.ts
@@ -0,0 +1,37 @@
+import { StoryFn } from '@storybook/vue3';
+import { StorybookControlEnum } from '../../common/storybook';
+import Card from './Card.vue';
+
+const meta = {
+  title: 'Card/Card',
+  component: Card,
+  argTypes: {
+    pressed: {
+      control: { type: StorybookControlEnum.boolean },
+      defaultValue: false
+    }
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+
+const Template: StoryFn = args => ({
+  components: { Card },
+  setup() {
+    return { args };
+  },
+  template: `
+        <Card v-bind="args" :class="{ pressed: args.pressed}" :style="{width: '162px', height: '140px'}">
+            Simple text
+        </Card>
+  `
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Pressed = Template.bind({});
+Pressed.args = {
+  pressed: true
+};
