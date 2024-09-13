b039340456f6a1b0e0433366ff2e54344d5a3239
Timofey Rumantsev <kg@re128.org>
2024-09-13 15:38:54 +0300

    fix: change cursor to pointer and font-size, click added

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -13,6 +13,8 @@
   box-shadow: 0px 4px 9.8px 0px #0000000D;
   background: white;
   border-radius: 15px;
+  cursor: pointer;
+  font-size: 14px;
   &:hover, &.pressed {
       border-color: #9CBEFF;
       border-style: solid;
