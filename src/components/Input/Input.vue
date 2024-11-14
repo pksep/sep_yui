d2a944860d900292078ffad8ea5788fc776e4bba
Konrad Geletey <39346864+kogeletey@users.noreply.github.com>
2024-11-14 17:07:15 +0300

    Sep-573 Input component (#39)
    * feat: start work on the input component
    
    * feat: supported fieldset of input
    
    * feat: start developed a close button
    
    * feat: now diffetent sizes and options are available, the general logic for the border is placed in a mixin
    
    * feat: now closing occurs only after input is entered and information about required
    
    * feat: inputTypes from browser new supported
    
    * feat: enum supported and types, fixed inputs
    
    * feat: add fonts, emit to input and change paddings
    
    * fix: eslint errors
    
    * fix: change height of sup, change font, paddings and focus
    
    * feat: support not height
    
    * feat: supported top
    
    * fix: types of input
    
    * fix focus and focusout in inputs
    
    * fix lint
    
    * fix controlled input
    
    * fix type event
    
    * fix type input event
    
    * add fontsource/source-sans-pro
    
    * fix styles
    https://app.weeek.net/ws/426401/project/3/board/15/m/task/2233
    https://app.weeek.net/ws/426401/project/3/board/15/m/task/2234
    
    * feat: refactor input ordinary dispaly
    
    * fix: padding, bottom and removed max-width
    
    * fix X position and delete move while hover
    
    * fix X position and delete move while hover
    
    * fix border move
    
    * fix border colors and legends
    
    * fix #4/1
    
    * fix #4/2
    
    * fix #5
    
    * adding global styles
    
    * adding function types
    
    * fix star position
    
    * fix star position
    
    * hide X when empty content
    
    ---------
    
    Co-authored-by: Edgar Knyazyan <edgarkn1999@gmail.com>

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,93 @@
+<template>
+  <fieldset
+    class="input-yui-kit"
+    :class="{
+      pressed: state.isPressed
+    }"
+    @focusout="handleBlur"
+  >
+    <legend class="input-yui-kit__legend">
+      {{ props.inputMessage
+      }}<sup class="input-yui-kit__star" v-if="props.required">*</sup>
+    </legend>
+    <input
+      ref="inputRef"
+      v-model="state.inputElement"
+      @focus="handleFocus"
+      @input="handleInput"
+      :type="props.type"
+      class="input-yui-kit__input"
+      :placeholder="props.placeholder"
+      :required="props.required"
+      input-message=""
+    />
+    <Button
+      :type="ButtonTypeEnum.ghost"
+      class="input-yui-kit__close"
+      @mousedown.prevent="clearInput"
+      v-if="state.isPressed && state.inputElement"
+    >
+      <Icon :name="IconNameEnum.exitSmall" color="currentColor" />
+    </Button>
+  </fieldset>
+</template>
+
+<script setup lang="ts">
+import { watch, reactive, ref } from 'vue';
+import type { IInputProps } from './interface/interface.ts';
+import Icon from './../Icon/Icon.vue';
+import Button from '../Button/Button.vue';
+import { ButtonTypeEnum } from '../Button/enum/enum';
+import { TextFieldEnum } from '../Input/enum/enum';
+import { IconNameEnum } from '../Icon/enum/enum';
+
+const emits = defineEmits<{
+  (e: 'input', value: string): void;
+}>();
+
+const props = withDefaults(defineProps<IInputProps>(), {
+  type: TextFieldEnum.text,
+  required: false
+});
+
+const state = reactive({
+  isPressed: false,
+  inputElement: ''
+});
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const clearInput = (): void => {
+  state.inputElement = '';
+  inputRef.value?.focus();
+};
+
+const handleInput = (e: Event): void => {
+  const target = e.currentTarget as HTMLInputElement;
+  emits('input', target.value);
+};
+
+const handleFocus = (): void => {
+  state.isPressed = true;
+};
+
+const handleBlur = (): void => {
+  state.isPressed = false;
+};
+
+watch(
+  () => state.inputElement,
+  value => {
+    state.isPressed = value?.length > 0;
+  }
+);
+</script>
+
+<style lang="scss" scoped>
+fieldset.input-yui-kit .input-yui-kit__close {
+  justify-self: end;
+  &:hover {
+    background-color: $TRANSPARENT;
+  }
+}
+</style>
