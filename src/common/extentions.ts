8e438fbf86e62e3e4e577529dc29607dac2a3f33
Edgar Knyazyan <edgarkn1999@gmail.com>
2024-12-02 14:50:42 +0400

    feat: add extentions for slider

--- src/common/extentions.ts
+++ src/common/extentions.ts
@@ -1,10 +1,13 @@
 export enum ImageExtensionsEnum {
   jpg = 'jpg',
   png = 'png',
-  jpeg = 'jpeg'
+  jpeg = 'jpeg',
+  webp = 'webp',
+  svg = 'svg'
 }
 
 export enum VideoExtensionsEnum {
   mp4 = 'mp4',
-  mp3 = 'mp3'
+  mp3 = 'mp3',
+  avif = 'avif'
 }
