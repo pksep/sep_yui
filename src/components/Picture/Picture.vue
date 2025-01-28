b5496d69656922dede1a39633d123488ab5f4ffa
kogeletey <kg@re128.org>
2025-01-28 14:01:02 +0300

    feat: new Picture compoennet with fallback caption

---
+++ src/components/Picture/Picture.vue
@@ -0,0 +1,44 @@
+<template>
+  <figure class="picture-yui-kit">
+    <template v-if="props.type === PictureEnum.img">
+      <picture class="picture-yui-kit__picture">
+        <img
+          class="picture-yui-kit__picture__image"
+          :src="props.url"
+          :alt="props.alt"
+        />
+      </picture>
+      <figcaption class="picture-yui-kit__picture__caption">
+        {{ props.caption }}
+      </figcaption>
+    </template>
+    <div class="picture-yui-kit__caption" v-else>
+      <h4 class="picture-yui-kit__caption__title">{{ props.caption }}</h4>
+    </div>
+  </figure>
+</template>
+
+<script setup lang="ts">
+import { PictureEnum } from './enums/enums';
+import type { IPictureProps } from './interface/interface';
+
+const props = withDefaults(defineProps<IPictureProps>(), {});
+</script>
+
+<style scoped>
+.picture-yui-kit__caption {
+  display: grid;
+  place-content: center;
+  border-radius: 15px;
+  background-color: var(--blue9);
+  & .picture-yui-kit__caption__title {
+    font-weight: bold;
+    font-size: 54px;
+    color: var(--text-blue);
+  }
+}
+
+.picture-yui-kit__picture__image {
+  width: 100%;
+}
+</style>
