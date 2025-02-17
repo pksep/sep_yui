163aba1d978c8564e766e25bc3aa477e26c71237
Timofey Rumantsev <kg@re128.org>
2025-02-17 10:39:39 +0300

    feat: installed v-calendar and styles

---
+++ src/components/Calendar/Calendar.stories.ts
@@ -0,0 +1,21 @@
+import { StoryFn, Meta } from '@storybook/vue3';
+import Calendar from './Calendar.vue';
+
+const meta = {
+  title: 'Calendar/Calendar',
+  component: Calendar,
+  argTypes: {},
+  tags: ['autodocs']
+} as Meta<typeof Calendar>;
+
+export default meta;
+
+const Template: StoryFn<typeof Calendar> = args => ({
+  components: { Calendar },
+  setup() {
+    return { args };
+  },
+  template: `<Calendar />`
+});
+
+export const Default = Template.bind({});
---
+++ src/components/Calendar/Calendar.vue
@@ -0,0 +1,9 @@
+<template>
+  <Calendar />
+</template>
+
+<script setup lang="ts">
+import { Calendar } from 'v-calendar';
+import 'v-calendar/style.css';
+</script>
+<style></style>
