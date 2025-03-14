90a92d8a084ec5a0f9bf13ae4d12119be284d717
Igor Grishchenko <sherkiosp@gmail.com>
2025-03-14 12:35:54 +0500

    #sep-10 feat: recreated tooltip

--- src/components/Tooltip/Tooltip.stories.ts
+++ src/components/Tooltip/Tooltip.stories.ts
@@ -5,7 +5,10 @@ const meta = {
   title: 'Tooltip/Tooltip',
   component: Tooltip,
   argTypes: {},
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  args: {
+    hint: 'Tooltip tooltip'
+  }
 } as Meta<typeof Tooltip>;
 
 export default meta;
@@ -15,10 +18,106 @@ const Template: StoryFn<typeof Tooltip> = args => ({
   setup() {
     return { args };
   },
-  template: `<Tooltip>
-    <template #default>Get Tooltip</template>
-    <template #tooltip>Tooltip</template>
-  </Tooltip>`
+  template: `
+  <div  style="height: 100px; display: flex; align-items:center; justify-content: center; ">
+    <Tooltip v-bind="args" >
+      Get Tooltip
+    </Tooltip>
+  </div>`
 });
 
 export const Default = Template.bind({});
+
+export const Black = Template.bind({});
+Black.args = {
+  isShow: true
+};
+
+export const White = Template.bind({});
+White.args = {
+  isShow: true,
+  type: 'white'
+};
+
+export const Blue = Template.bind({});
+Blue.args = {
+  isShow: true,
+  type: 'blue'
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  isShow: true
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  isShow: true,
+  size: 'medium'
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  isShow: true,
+  size: 'large'
+};
+
+export const BottomRight = Template.bind({});
+BottomRight.args = {
+  position: 'bottom-right',
+  isShow: true
+};
+
+export const TopCenter = Template.bind({});
+TopCenter.args = {
+  position: 'top-center',
+  isShow: true
+};
+
+export const TopLeft = Template.bind({});
+TopLeft.args = {
+  position: 'top-left',
+  isShow: true
+};
+
+export const TopRight = Template.bind({});
+TopRight.args = {
+  position: 'top-right',
+  isShow: true
+};
+
+export const LeftCenter = Template.bind({});
+LeftCenter.args = {
+  position: 'left-center',
+  isShow: true
+};
+
+export const LeftTop = Template.bind({});
+LeftTop.args = {
+  position: 'left-top',
+  isShow: true
+};
+
+export const LeftBottom = Template.bind({});
+LeftBottom.args = {
+  position: 'left-bottom',
+  isShow: true
+};
+
+export const RightCenter = Template.bind({});
+RightCenter.args = {
+  position: 'right-center',
+  isShow: true
+};
+
+export const RightTop = Template.bind({});
+RightTop.args = {
+  position: 'right-top',
+  isShow: true
+};
+
+export const RightBottom = Template.bind({});
+RightBottom.args = {
+  position: 'right-bottom',
+  isShow: true
+};
--- src/components/Tooltip/interface/interface.ts
+++ src/components/Tooltip/interface/interface.ts
@@ -1,3 +1,23 @@
+export type TTooltipPosition =
+  | 'bottom-right'
+  | 'bottom-left'
+  | 'bottom-center'
+  | 'top-right'
+  | 'top-left'
+  | 'top-center'
+  | 'left-top'
+  | 'left-center'
+  | 'left-bottom'
+  | 'right-top'
+  | 'right-center'
+  | 'right-bottom';
+export type TTooltipSize = 'small' | 'medium' | 'large';
+export type TTooltipType = 'white' | 'black' | 'blue';
+
 export interface ITooltipProps {
+  hint: string;
   isShow?: boolean;
+  position?: TTooltipPosition;
+  size?: TTooltipSize;
+  type?: TTooltipType;
 }
