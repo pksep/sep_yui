691bc1ca0993119dd74713931c7f650b8a4e52a7
kogeletey <kg@re128.org>
2025-04-24 08:26:27 +0300

    fix: ishow working correctly

--- src/components/Popover/interface/interface.ts
+++ src/components/Popover/interface/interface.ts
@@ -2,17 +2,17 @@ import { IconNameEnum } from '../../Icon/enum/enum';
 import { PopoverHoverEnum } from '../enums/enums';
 
 export interface IPopoverOption {
-    value: string;
-    function: () => void;
+  value: string;
+  function: () => void;
 }
 
 export interface IPopoverProps {
-    isShow?: boolean;
-    iconName?: IconNameEnum;
-    options: IPopoverOption[];
-    tooltip?: string;
+  isShow?: boolean;
+  iconName?: IconNameEnum;
+  options: IPopoverOption[];
+  tooltip?: string;
 }
 
 export interface IPopoverHoverProps {
-    position: PopoverHoverEnum;
+  position: PopoverHoverEnum;
 }
