e04aa5c469174a0d2f6a3632a722658b951a0634
kogeletey <kg@re128.org>
2025-11-04 10:57:02 +0300

    fix: change width screen of mobile content editor

--- src/components/ContentEditor/ContentEditor.vue
+++ src/components/ContentEditor/ContentEditor.vue
@@ -328,7 +328,7 @@ button.mobile-buttons {
   height: 0;
 }
 
-@media screen and (width <= 450px) {
+@media screen and (width <= 28rem) {
   button.ghost-yui-kit.mobile-buttons {
     display: grid;
     background-color: transparent !important;
