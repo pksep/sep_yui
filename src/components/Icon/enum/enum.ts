4c1e2716f518d28e1cc605f7d64c6d9bb48721cb
kogeletey <kg@re128.org>
2025-12-19 11:12:39 +0300

    feat: delivered and notDelivered icon add

--- src/components/Icon/enum/enum.ts
+++ src/components/Icon/enum/enum.ts
@@ -110,5 +110,7 @@ export enum IconNameEnum {
   message = 'message',
   darkThemeSwitcher = 'dark-theme-switcher',
   qrCode = 'qr-code',
-  distrub = 'distrub'
+  distrub = 'distrub',
+  delivered = 'delivered',
+  notDelivered = 'notDelivered'
 }
